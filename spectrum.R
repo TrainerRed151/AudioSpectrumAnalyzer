5a6d60946b604d96250d28cc4f44c91bbfab3418
Brian Pomerantz <bbp114@psu.edu>
2015-04-15 23:41:11 -0400

    R file for testing

---
+++ spectrum.R
@@ -0,0 +1,28 @@
+library(tuneR)
+
+music <- readWave("violin.wav")
+
+#s <- 90000
+
+dx <- 1024
+
+fs <- music@samp.rate
+
+xx <- seq(0, dx)*fs/dx
+
+f <- seq(5, 14)
+f2 <- 2^f
+ff <- f2*dx/fs + 1
+fi <- floor(ff)
+
+
+spectrum <- function(s) {
+    ps <- fft(music@left[s:(s+dx)])
+    #plot(xx, abs(ps), type="l", xlim=c(32,16384), log="xy")
+    sa <- 10*log10(abs(ps[fi]))
+    print(sa)
+}
+
+for (i in seq(1, length(music@left)-dx, dx)) {
+    spectrum(i)
+}
